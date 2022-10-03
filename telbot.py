63de43206e26014d5cce0c66562d1aeb4fba6ef8
ssktelegram <ssktelegram@gmail.com>
2022-10-03 13:51:34 +0530

    4nd commit

--- telbot.py
+++ telbot.py
@@ -8,14 +8,16 @@ client = TelegramClient('Deals', api_id, api_hash)
 
 @client.on(events.NewMessage)
 async def handler(event):
+    chat = await event.get_chat()
     chatid=event.chat_id    
     print("chatid")
     print(chatid)
     print("event")
     print(event)
-    
-    # path = await client.download_media(event.media, "./photo")
-    # print('File saved to', path)
+    print()
+    print(chat)
+
+
     if chatid==732565199:#(GrowDeals)
        msg=event.raw_text
        await client.send_message(997904331,msg)
@@ -23,10 +25,10 @@ async def handler(event):
     if chatid==-1001160797877 or -1001293749899 or -1001450755585 or -1001420742409:
                 #(Price Histry detail) or (GrowDeals) or (TrindingDeals) or (TrueGrabbers)
         msg=event.raw_text
-        try:
-         await client.send_message(5739096966,msg)
-        except ValueError :
-            print(ValueError.args)
+        # try:
+        await client.send_message(5739096966,msg)
+        # except e :
+        #     print(e)
 
     # if chatid==-1001293749899:#(GrowDeals)
     #     msg=event.raw_text
