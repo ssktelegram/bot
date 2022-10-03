55b3dd94af1d6fd3a2844b949a533c93c9ea843c
ssktelegram <ssktelegram@gmail.com>
2022-10-03 13:04:12 +0530

    3nd commit

--- telbot.py
+++ telbot.py
@@ -10,56 +10,35 @@ client = TelegramClient('Deals', api_id, api_hash)
 async def handler(event):
     chatid=event.chat_id    
     print("chatid")
-
     print(chatid)
     print("event")
     print(event)
     
-    path = await client.download_media(event.media, "./photo")
-    print('File saved to', path)
+    # path = await client.download_media(event.media, "./photo")
+    # print('File saved to', path)
     if chatid==732565199:#(GrowDeals)
-        msg="Ha"+event.raw_text
-        if path!= None:
-         await client.send_file(732565199,file=path,caption=msg)
-         os.remove(path)
+       msg=event.raw_text
+       await client.send_message(732565199,msg)
          
     if chatid==-1001160797877:#(Price Histry detail)
         msg=event.raw_text
-        # if path!= None:
-        #  await client.send_file(997904331,path)
-        #  os.remove(path)
+        await client.send_message(5739096966,msg)
 
-        await client.send_message (5739096966,msg)
-    # if chatid==-1001356817628:#(Loot Deals)
-    #     msg=event.raw_text
-    #     # if path!= None:
-    #     #  await client.send_file(997904331,path)
-    #     #  os.remove(path)
-
-    #     await client.send_message (5739096966,msg)
     if chatid==-1001293749899:#(GrowDeals)
         msg=event.raw_text
-        if path!= None:
-         await client.send_file(5739096966,path,caption=msg)
-         os.remove(path)
+        await client.send_message(5739096966,msg)
 
     if chatid==997904331:#(GrowDeals)
         msg=event.raw_text
-        if path!= None:
-         await client.send_file(997904331,path,caption=msg)
-         os.remove(path)
+        await client.send_message(5739096966,msg)
 
     if chatid==-1001450755585:#(TrindingDeals)
         msg=event.raw_text
-        if path!= None:
-         await client.send_file(5739096966,path,caption=msg)
-         os.remove(path)
+        await client.send_message(5739096966,msg)
 
     if chatid==-1001420742409:#(TrueGrabbers)
         msg=event.raw_text
-        if path!= None:
-         await client.send_file(5739096966,path,caption=msg)
-         os.remove(path)
+        await client.send_message(5739096966,msg)
 
 
 
