130b3561ad3305e95bd751aee817afe2d2f0af9f
ssktelegram <ssktelegram@gmail.com>
2022-10-03 13:19:50 +0530

    4nd commit

--- telbot.py
+++ telbot.py
@@ -18,28 +18,42 @@ async def handler(event):
     # print('File saved to', path)
     if chatid==732565199:#(GrowDeals)
        msg=event.raw_text
-       await client.send_message(732565199,msg)
+       await client.send_message(997904331,msg)
          
     if chatid==-1001160797877:#(Price Histry detail)
         msg=event.raw_text
-        await client.send_message(5739096966,msg)
+        try:
+         await client.send_message(5739096966,msg)
+        except ValueError:
+            print(ValueError)
 
     if chatid==-1001293749899:#(GrowDeals)
         msg=event.raw_text
-        await client.send_message(5739096966,msg)
+        try:
+         await client.send_message(5739096966,msg)
+        except ValueError:
+            print(ValueError)
 
     if chatid==997904331:#(GrowDeals)
         msg=event.raw_text
-        await client.send_message(5739096966,msg)
-
+        try:
+         await client.send_message(5739096966,msg)
+        except ValueError:
+            print(ValueError)
+            
     if chatid==-1001450755585:#(TrindingDeals)
         msg=event.raw_text
-        await client.send_message(5739096966,msg)
-
+        try:
+         await client.send_message(5739096966,msg)
+        except ValueError:
+            print(ValueError)
+            
     if chatid==-1001420742409:#(TrueGrabbers)
         msg=event.raw_text
-        await client.send_message(5739096966,msg)
-
+        try:
+         await client.send_message(5739096966,msg)
+        except ValueError:
+            print(ValueError)
 
 
 client.start()
