6c85010f12bab5869503dd84bb564cc0e78921ac
ssktelegram <ssktelegram@gmail.com>
2022-10-18 14:53:49 +0530

    affeleters added with img2

--- telbot.py
+++ telbot.py
@@ -1,4 +1,4 @@
-from asyncio.windows_events import NULL
+# from asyncio.windows_events import NULL
 from telethon import TelegramClient, events
 import os
 
@@ -20,7 +20,27 @@ async def handler(event):
     # print(chat)
     path = await client.download_media(event.media, "./photo")
     print('File saved to', path)  # printed after download is done
-    if (path == NULL):
+    if (path):
+       if chatid == 997904331:  # (Price Histry detail)7#997904331:#(me)
+        msg = event.raw_text
+       #    await client.send_message(5739096966,msg)
+        await client.send_file(1485109749,path,caption=msg)
+       if chatid == -1001160797877:  # (Price Histry detail)7#997904331:#(me)
+        msg = event.raw_text
+        await client.send_file(1485109749,path,caption=msg)
+       if chatid == 732565199:  # (test)7#997904331:#(me)
+        msg = event.raw_text
+        await client.send_file(1485109749,path,caption=msg)
+       if chatid == -1001293749899:  # (GrowDeals)7#997904331:#(me)
+        msg = event.raw_text
+        await client.send_file(1485109749,path,caption=msg)
+       if chatid == -1001450755585:  # (TrindingDeals)7#997904331:#(me)
+        msg = event.raw_text
+        await client.send_file(1485109749,path,caption=msg)
+       if chatid == -1001420742409:  # (TrueGrabbers)7#997904331:#(me)
+        msg = event.raw_text
+        await client.send_file(1485109749,path,caption=msg)
+    else:
        if chatid == 997904331:  # (Price Histry detail)7#997904331:#(me)
         msg = event.raw_text
        #    await client.send_message(5739096966,msg)
@@ -40,27 +60,11 @@ async def handler(event):
        if chatid == -1001420742409:  # (TrueGrabbers)7#997904331:#(me)
         msg = event.raw_text
         await client.send_message(1485109749, msg)
-    else:
-       if chatid == 997904331:  # (Price Histry detail)7#997904331:#(me)
-        msg = event.raw_text
-       #    await client.send_message(5739096966,msg)
-        await client.send_file(1485109749,path,caption=msg)
-       if chatid == -1001160797877:  # (Price Histry detail)7#997904331:#(me)
-        msg = event.raw_text
-        await client.send_file(1485109749,path,caption=msg)
-       if chatid == 732565199:  # (test)7#997904331:#(me)
-        msg = event.raw_text
-        await client.send_file(1485109749,path,caption=msg)
-       if chatid == -1001293749899:  # (GrowDeals)7#997904331:#(me)
-        msg = event.raw_text
-        await client.send_file(1485109749,path,caption=msg)
-       if chatid == -1001450755585:  # (TrindingDeals)7#997904331:#(me)
-        msg = event.raw_text
-        await client.send_file(1485109749,path,caption=msg)
-       if chatid == -1001420742409:  # (TrueGrabbers)7#997904331:#(me)
-        msg = event.raw_text
-        await client.send_file(1485109749,path,caption=msg)
-
+       
+       
+       
+       
+       
     # if chatid==-1001160797877 or -1001293749899 or -1001450755585 or -1001420742409:
     #             #(Price Histry detail) or (GrowDeals) or (TrindingDeals) or (TrueGrabbers)
     #     msg=event.raw_text
