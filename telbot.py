5d5fb66d1b5028835620d9b0d87cc241f75b5c26
ssktelegram <ssktelegram@gmail.com>
2023-01-11 15:27:29 +0530

    done

--- telbot.py
+++ telbot.py
@@ -2,13 +2,13 @@
 from telethon import TelegramClient, events
 import os
 
-import urllib
-import requests
-key = 'fe5df5f79db41efb1b79be5befe337d0c8ea8'
-def shorttheurl(url):
-    r=f'http://cutt.ly/api/api.php?key={key}&short={url}'
-    res=requests.get(r).json()['url']
-    return res['shortLink']
+# import urllib
+# import requests
+# key = 'fe5df5f79db41efb1b79be5befe337d0c8ea8'
+# def shorttheurl(url):
+#     r=f'http://cutt.ly/api/api.php?key={key}&short={url}'
+#     res=requests.get(r).json()['url']
+#     return res['shortLink']
     
 
 
